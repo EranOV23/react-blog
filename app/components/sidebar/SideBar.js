dd29e65466c6d87759bce4d7833c8e9bf1e04008
Eran Ovadia <erano@netcraft.local>
2017-11-08 14:49:38 +0200

    remove search from sidebar

--- app/components/sidebar/SideBar.js
+++ app/components/sidebar/SideBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Search from './Search';
 import Filters from './Filters';
 
 import './sidebar.scss'
@@ -9,10 +8,7 @@ export default class SideBar extends React.Component{
   render(){
     return(
       <div className="sidebar">
-
-        <Search/>
         <Filters/>
-
       </div>
     )
   }

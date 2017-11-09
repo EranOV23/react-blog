64ccc07751c1c289c4f6aa7036859f34ede6d7b1
EranOV23 <eran.ovadia123@gmail.com>
2017-11-09 13:46:28 +0200

    edit travis

--- server/app.js
+++ server/app.js
@@ -6,22 +6,22 @@ let app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// let posts = require("../data/postsList");
-//
-// app.route("/api/posts")
-//   .get((req, res) => {
-//     console.log('Requested node server');
-//     res.json(posts);
-//   })
-//   .post((req,res)=>{
-//     let newPost = req.body;
-//     let postsArr = posts.posts;
-//     req.body.tags = req.body.tags.split(",");
-//     postsArr.push(req.body);
-//     console.log(`post: ${req.body.title}`);
-//     res.writeHead(302, {'Location': '/#/admin'});
-//     res.end();
-//   });
+let posts = require("../data/postsList");
+
+app.route("/api/posts")
+  .get((req, res) => {
+    console.log('Requested node server');
+    res.json(posts);
+  })
+  .post((req,res)=>{
+    let newPost = req.body;
+    let postsArr = posts.posts;
+    req.body.tags = req.body.tags.split(",");
+    postsArr.push(req.body);
+    console.log(`post: ${req.body.title}`);
+    res.writeHead(302, {'Location': '/#/admin'});
+    res.end();
+  });
 
 
 app.listen(9090);

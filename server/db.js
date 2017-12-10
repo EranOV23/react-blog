99885b23a18d340b62b177c39547e11c88de3050
Eran Ovadia <erano@netcraft.local>
2017-12-10 16:29:39 +0200

    connect mongoDB to mLab hosting

--- server/db.js
+++ server/db.js
@@ -2,6 +2,6 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-mongoose.connection.openUri("mongodb://localhost:27017/blog");
+mongoose.connection.openUri("mongodb://eranov23:123456@ds135946.mlab.com:35946/react-blog");
 
 module.exports = mongoose;
